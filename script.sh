4dc6f343dc1912f921aff5b9878389c5a2b89047
alexisgeorges <11684986+alexisgeorges@users.noreply.github.com>
2020-02-07 15:35:12 +0000

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -u -e -o pipefail
+
+readonly currentDir=$(
+    cd $(dirname $0)
+    pwd
+)
+readonly SCRIPTS_LOCATION=$currentDir
+readonly rootDir="${currentDir}/.."
+
+echo "this is a test" > $rootDir/test.md
+
+git add $rootDir/.
+git commit -m "this is a test"
